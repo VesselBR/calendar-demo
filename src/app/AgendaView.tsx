ad74df8c4da74636aad01921f3c78ce0e1e97212
Reinaldo Costa <reinaldoacdc@gmail.com>
2024-09-26 20:46:24 -0300

    Set events

--- src/app/AgendaView.tsx
+++ src/app/AgendaView.tsx
@@ -3,14 +3,18 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment-timezone'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
-
+import { useState } from 'react'
+import events from "./events"
 export type AgendaViewProps = {
     date: Date
 }
 
 
 export default function AgendaView(props: AgendaViewProps) {
+    const [myEvents, setMyEvents] = useState(events)
+
     const DnDCalendar = withDragAndDrop(Calendar)
+    moment.tz.setDefault('America/Sao_Paulo')
     const localizer = momentLocalizer(moment)
 
     return (
@@ -20,8 +24,10 @@ export default function AgendaView(props: AgendaViewProps) {
             date={props.date}
             defaultView={Views.DAY}
             localizer={localizer}
-            events={[]}
+            events={myEvents}
             views={{day: true, month: true}}
+            min={new Date(new Date().setHours(8, 0, 0))}
+            max={new Date(new Date().setHours(20, 0, 0))}
         />        
         </>
     )
